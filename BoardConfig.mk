4658542fd67d93b5089e467a6a21244f2f677e1b
Corinna Vinschen <xda@vinschen.de>
2017-11-05 13:36:10 +0100

    trlte: Define TW_NO_LEGACY_PROPS to allow installing Oreo
    Change-Id: Ibf8fdc3462953d08b313372a8a2e2ad03b4c876b
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,3 +88,6 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_trlte
 TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_trlte.cpp
+
+# Oreo
+TW_NO_LEGACY_PROPS := true
